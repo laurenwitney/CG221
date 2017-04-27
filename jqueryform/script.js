68ff89baff19127b4d384fa9db2616520ceabf99
laurenwitney <lauren_witney@fitnyc.edu>
2017-04-26 21:36:57 -0400

    Add files via upload

---
+++ jqueryform/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  //alert("You made it!");
+
+  //$('img').hover(function(){
+    //$(this).fadeOut(1000);
+    //$(this).fadeIn(1000);
+  //});
+
+  $("div").hover(function(){
+       $('img').animate({left: '400px'});
+    });
+
+});
